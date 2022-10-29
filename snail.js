4de220f2923b260c9e24415a4d933a766a7cd8ed
SnailBurner <114715436+SnailBurner@users.noreply.github.com>
2022-10-29 16:54:08 +0800

    Create snail.js

---
+++ snail.js
@@ -0,0 +1,18 @@
+
+
+{
+    let weakness = false;
+    let richness = false;
+     while(true)
+    {
+        weakness = (ns.getServerSecurity(target) == ns.getServerMinSecurity(target));
+        richness = (ns.getServerAvailableMoney(target) == ns.getServerMaxMoney(target));
+        if (weakness && richness)
+    
+    
+    }
+
+
+
+
+}
