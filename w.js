66bc94a75f5f964faffa4c4677a7fc63eb4b1610
SnailBurner <114715436+SnailBurner@users.noreply.github.com>
2024-02-04 16:00:53 +0800

    Create w.js

---
+++ w.js
@@ -0,0 +1,11 @@
+/** @param {NS} ns */
+// code by SnailBurner.
+export async function main(ns)
+{
+	let target = ns.args[0];
+	let threadCount = ns.args[1];			//integer between 1 and the amout of threads the script is run with
+	let pump = ns.args[2]; 						//true or false to rig the market 
+	let delay = ns.args[3];						//in ms
+	await ns.weaken(target, {threads: threadCount, stock: pump, additionalMsec: delay});
+
+}
