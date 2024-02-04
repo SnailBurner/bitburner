5a65d01f5fd83f64fc64b5a430742d967d3fedf6
SnailBurner <114715436+SnailBurner@users.noreply.github.com>
2024-02-04 16:04:06 +0800

    Delete batcher0 directory

--- batcher0/g00.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns */
-// code by SnailBurner.
-export async function main(ns)
-{
-	let target = ns.args[0];
-	await ns.grow(target);
-}
--- batcher0/w00.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns */
-// code by SnailBurner.
-export async function main(ns)
-{
-	let target = ns.args[0];
-	let x = ns.args[1];
-	await ns.weaken(target);
-}
